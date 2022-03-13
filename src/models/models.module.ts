863624c12fc2828316dbf73fd67a60cffa4b5c73
kabilan21 <kabilankabil220@gmail.com>
2022-03-13 21:31:32 +0530

    reolved dependencies

--- src/models/models.module.ts
+++ src/models/models.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UtilsService } from 'src/utils/utils.service';
 import { Course } from './entities/course.entity';
+import { FillInTheBlanks, FillResponse } from './entities/fill.entity';
 import { MatchAnswer } from './entities/match-answer.entity';
 import { MatchQuestion } from './entities/match-question.entity';
 import { MatchResponse } from './entities/match-response.entity';
@@ -11,12 +12,17 @@ import { MultipleChoice } from './entities/multiple-choice.entity';
 import { Staff } from './entities/staff.entity';
 import { Student } from './entities/student.entity';
 import { User } from './entities/user.entity';
+import { Video, VideoResponse } from './entities/video.entity';
 import { CourseService } from './services/course.service';
+import { FillintheblanksService } from './services/fillintheblanks.service';
+import { FillintheBlanksResponseService } from './services/fillintheblanksresponse.service';
 import { MatchService } from './services/match.service';
 import { MulitpleChoiceService } from './services/mulitplechoice.service';
 import { StaffService } from './services/staff.service';
 import { StudentService } from './services/student.service';
 import { UserService } from './services/user.service';
+import { VideoResponseService } from './services/video-response.service';
+import { VideoService } from './services/video.service';
 
 @Module({
   imports: [
@@ -40,10 +46,35 @@ import { UserService } from './services/user.service';
       MatchEntity,
       MatchAnswer,
       MatchQuestion,
-      MatchResponse]),
+      MatchResponse,
+      FillInTheBlanks,
+      FillResponse,
+      VideoResponse,
+      Video,
+    ]),
   ],
   providers: [
-    StaffService, UserService, StudentService, UtilsService, MulitpleChoiceService, CourseService, MatchService],
-  exports: [StaffService, UserService, StudentService, MulitpleChoiceService, CourseService, MatchService],
+    StaffService,
+    UserService,
+    StudentService,
+    UtilsService,
+    MulitpleChoiceService,
+    CourseService,
+    MatchService,
+    VideoResponseService,
+    FillintheBlanksResponseService,
+    VideoService,
+    FillintheblanksService],
+  exports: [
+    StaffService,
+    UserService,
+    StudentService,
+    MulitpleChoiceService,
+    CourseService,
+    MatchService,
+    VideoResponseService,
+    VideoService,
+    FillintheBlanksResponseService,
+    FillintheblanksService],
 })
 export class ModelsModule { }
