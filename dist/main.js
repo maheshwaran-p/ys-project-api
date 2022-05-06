1a0d522e437ce56222769b39aa3483ef51c2cda7
maheshwaran-p <mpmahesh1022@gmail.com>
2022-05-06 14:14:23 +0530

    cors sloved

--- dist/main.js
+++ dist/main.js
@@ -4,6 +4,10 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule, { cors: true });
+    app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        next();
+    });
     await app.listen(process.env.PORT || 4000);
     app.enableCors();
 }
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule,{cors:true});
   
   
-
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
   await app.listen(process.env.PORT|| 4000);
 
   app.enableCors();
